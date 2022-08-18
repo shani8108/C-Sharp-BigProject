6f54de6053a9b62a6bba8572340770888557d08f
shani8108 <148148sh@gmail.com>
2022-08-18 15:55:13 +0300

    Commit PartialClass

---
+++ ShowBigProject/Meet3-PartialClass1_partial_1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowBigProject
+{
+    internal partial class Meet3_PartialClass1
+    {
+        //your code
+    }
+}
---
+++ ShowBigProject/Meet3-PartialClass1_partial_2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowBigProject
+{
+    internal partial class Meet3_PartialClass1
+    {
+        //we can defined it also here because we have another class in another file
+        //so put partial
+        public void Run()
+        {
+            //automatic_wizard
+        }
+    }
+}
