be0689c7dab3acb3e385a4eed73dd942f1e060e0
shani8108 <148148sh@gmail.com>
2022-11-06 22:03:40 +0200

    Commit Type-Finalizer

---
+++ ShowBigProject/Meet7-Type.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowBigProject
+{
+    internal class Meet7_Type
+    {
+        FileStream fs;
+        public void Run()
+        {
+            try
+            {
+                fs = System.IO.File.OpenRead("");
+            }catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                fs.Close();
+            }
+
+
+
+           // fs.Close();
+        }
+        public Meet7_Type()
+        {
+            //constructor
+        }
+        ~Meet7_Type()
+        {
+            //desctructor or finalizer
+            fs.Close(); 
+        }
+    }
+}
